2b3b48915c66e8fdb012dbdc0c8a60d9e4b465ff
İlker Ata Şahin <ilkeratasahin@gmail.com>
2024-09-30 02:03:06 +0300

    project init

---
+++ src/main/java/dev/ias/springtextboard/SpringTextboardApplication.java
@@ -0,0 +1,13 @@
+package dev.ias.springtextboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTextboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTextboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ias/springtextboard/SpringTextboardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ias.springtextboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTextboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
